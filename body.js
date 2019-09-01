e1622ad849a562ad1454d2f0eb47cc012d22537e
Joey Romo <joeyjr95@gmail.com>
2019-08-31 20:45:01 -0400

    rough paragraph layout

--- body.js
+++ body.js
@@ -27,7 +27,9 @@ const body = function(){
             <div class=bio-body>
               <section class="about-me">
                 <h3>About Me</h3>
-                <p>Currently I am a 23 year old Full-Stack web development student at Thinkful from Washington DC. As a developer in training, I love learning different code everyday and eventually putting it together to make projects. I have an inescapable attention to detail, which sometimes means spending more time than I should adjusting pixels in CSS. I enjoy building and designing things, which brought me to Thinkful and programming in general. Before I started my dive into programming I was a business administration student at Flagler College, but realized business administration wasn’t for me. Besides being a student, I’ve been a dairy farmer in New Zealand, tradie in Australia, and a valet in DC. Besides programming, I enjoy hiking, photography, binging Twin Peaks on Netflix, watching the Washington Wizards disappoint me every year,  and cheap beer.
+                <p>Currently I am a 23 year old Full-Stack web development student at Thinkful from Washington DC. As a developer in training, I love learning different code everyday and eventually putting it together to make projects. I have an inescapable attention to detail, which sometimes means spending more time than I should adjusting pixels in CSS. I enjoy building and designing things, which brought me to Thinkful and programming in general. </p>
+                <p>Before I started my dive into programming I was a business administration student at Flagler College, but realized business administration wasn’t for me. Besides being a student, I’ve been a dairy farmer in New Zealand, tradie in Australia, and a valet in DC. </p>
+                <p>Other than programming, I enjoy hiking, photography, binging Twin Peaks on Netflix, watching the Washington Wizards disappoint me every year,  and cheap beer.
                   </p>
               </section>
             </div>
